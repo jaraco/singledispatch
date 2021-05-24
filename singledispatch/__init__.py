b7ae3d2d00dcf2e27eb486c7022d27305e70d53d
James Morris <jamesmorris2@gmail.com>
2021-05-23 22:04:25 -0400

    Add helper function to validate annotations for registration

--- singledispatch/__init__.py
+++ singledispatch/__init__.py
@@ -164,6 +164,24 @@ def _find_impl(cls, registry):
             match = t
     return registry.get(match)
 
+def _validate_annotation(func):
+    """Determine if an annotation is valid for registration.
+
+    An annotation is considered valid for use in registration if it is an
+    instance of ``type`` and not a generic type from ``typing``.
+    """
+    argname, cls = next(iter(get_type_hints(func).items()))
+    try:
+        # In Python 3.5 and 3.6, the classes in typing are considered instances
+        # of type, but they aren't valid for registering single dispatch
+        # functions so we need to check against TypingMeta instead
+        from typing import GenericMeta
+        valid = not isinstance(cls, GenericMeta)
+    except ImportError:
+        # In Python 3.7+, classes in typing are not instances of type
+        valid = isinstance(cls, type)
+    return argname, cls, valid
+
 def singledispatch(func):
     """Single-dispatch generic function decorator.
 
@@ -217,9 +235,8 @@ def singledispatch(func):
                     "on an annotated function.".format(**locals())
                 )
             func = cls
-
-            argname, cls = next(iter(get_type_hints(func).items()))
-            if not isinstance(cls, type) or hasattr(cls, '_gorg'):
+            argname, cls, valid = _validate_annotation(func)
+            if not valid:
                 raise TypeError(
                     "Invalid annotation for {argname!r}. "
                     "{cls!r} is not a class.".format(**locals())
