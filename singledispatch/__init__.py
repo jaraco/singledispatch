e089a8ff30048c058a6377911ebd6b04538b5d5b
James Morris <jamesmorris2@gmail.com>
2021-06-12 17:09:44 -0400

    Add singledispatchmethod to __all__

--- singledispatch/__init__.py
+++ singledispatch/__init__.py
@@ -6,7 +6,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__all__ = ['singledispatch']
+__all__ = ['singledispatch', 'singledispatchmethod']
 
 from weakref import WeakKeyDictionary
 
